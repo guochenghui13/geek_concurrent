f8abf914d268734df6dc66a95728db50c940ebf7
guochenghui <guochenghui@fenbi.com>
2021-11-02 12:04:50 +0800

    init

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ * @(#)HelloWorld.java, 11月 02, 2021.
+ * <p>
+ * Copyright 2021 fenbi.com. All rights reserved.
+ * FENBI.COM PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+/**
+ * @author guochenghui
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello World");
+    }
+}
